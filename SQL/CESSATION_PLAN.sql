7f6e4b2fe2a273ce5d2b1ea0376b65e46f46d7c7
chinh140104 <chinhhtse183685@fpt.edu.vn>
2025-06-13 11:09:31 +0700

    pageHome_antd_footer

--- SQL/CESSATION_PLAN.sql
+++ SQL/CESSATION_PLAN.sql
@@ -3,7 +3,7 @@
 ALTER TABLE CESSATION_PLAN DROP COLUMN month_quit;
 
 -- Thêm cột month_quit mới kiểu INT, NOT NULL (có thể thêm DEFAULT nếu cần)
-ALTER TABLE CESSATION_PLAN ADD month_quit INT NOT NULL DEFAULT 1;
+ALTER TABLE CESSATION_PLAN ADD end_date DATE NULL;
 
 -- Xóa bản ghi cũ của user_id = 2
 DELETE FROM CESSATION_PLAN
