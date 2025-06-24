c275adf62eec729732af1eda870297abcda8e8de
myProject <anhntse183220@fpt.edu.vn>
2025-06-24 08:22:00 +0700

    API Update,Delete Comment

--- BackEnd/routes/comment.js
+++ BackEnd/routes/comment.js
@@ -4,7 +4,16 @@ const router = express.Router();
 const { auth } = require('../middleware/auth');
 const commentController = require('../controllers/commentController');
 
+// Thêm comment
 router.post('/', auth, commentController.createComment);
+
+// Lấy danh sách comment theo bài viết
 router.get('/:postId', commentController.getCommentsByPost);
+
+// Update comment
+router.put('/:id', auth, commentController.updateComment);
+
+// Delete comment
+router.delete('/:id', auth, commentController.deleteComment);
 
 module.exports = router;
